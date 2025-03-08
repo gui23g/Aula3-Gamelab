e1ae561ed0bf51fab3396b81d38bb23b936e8a85
Gustavo Colombini <gucolombini@gmail.com>
2025-03-08 04:30:03 -0300

    comenta umas coisa bonitinha ai pra n fica mt feio o codigo

--- src/scenes/maze.js
+++ src/scenes/maze.js
@@ -6,34 +6,34 @@ class Maze extends Phaser.Scene {
     }
 
     preload(){
-      // load the PNG file
-      this.load.image('mazetileset', 'src/assets/mazetileset.png')
-      // load the JSON file
-      this.load.tilemapTiledJSON('mazetilemap', 'src/assets/maze.json')
+      this.load.image('mazetileset', 'src/assets/mazetileset.png') // carrega TEXTURAS do mapa
+      this.load.tilemapTiledJSON('mazetilemap', 'src/assets/maze.json') // carrega INFORMAÇÕES do mapa
+
       this.load.spritesheet('player', 'src/assets/robophaser.png', { frameWidth: 28, frameHeight: 38 });
+      this.load.spritesheet('engrenagem', 'src/assets/engrenagem.png', { frameWidth: 32, frameHeight: 32 });
     }
 
     create(){
-      //this.add.image(50, 50, 'mazetileset')
-      createAnimation(this, 'walk', 'player', 0, 3, 3, -1);
-      createAnimation(this, 'idle', 'player', 1, 1, -1, -1); // funcao gostosa dmais vc eh muito lindo
       const map = this.make.tilemap({ key: 'mazetilemap' })
 
-      // add the tileset image we are using
       const tileset = map.addTilesetImage('mazetileset', 'mazetileset')
       
+      // criar cada camada presente no mapa do tiled na ordem de baixo para cima
       map.createStaticLayer('chao', tileset)
       const colisaoLayer = map.createStaticLayer('colisao', tileset)
-      colisaoLayer.setCollisionByProperty({ collides: true });
-      //colisaoLayer.setCollisionBetween(1, 50);
+      colisaoLayer.setCollisionByProperty({ collides: true }); // todos os tiles com propriedade collides: true vao colidir nessa camada
+      //colisaoLayer.setCollisionBetween(1, 50); // método alternativo de setar colisões
       map.createStaticLayer('decoracao', tileset)
 
-      this.player = this.physics.add.sprite(100, 100, 'player');
+      createAnimation(this, 'andar', 'player', 0, 3, 3, -1);
+      createAnimation(this, 'parado', 'player', 1, 1, -1, -1); // funcao gostosa dmais gui vc eh muito lindo
+      createAnimation(this, 'engrenagemgira', 'engrenagem', 0, 3, 3, -1);
 
-      this.physics.add.collider(this.player, colisaoLayer);
+      this.player = this.physics.add.sprite(100, 100, 'player');
+      this.physics.add.collider(this.player, colisaoLayer); // seta colisão do player com a camada do mapa
     }
 
     update(){
-        moveChar(this, 200, 200, this.player); // economizei 3929832999 linhas de codigo aq
+        moveChar(this, 200, 200, this.player); // economizei 3929832999 linhas de codigo aq tmj
     }
 }
