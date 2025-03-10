525c63b8a0c8b27271f83abb20b686822ec2308c
Gustavo Colombini <gucolombini@gmail.com>
2025-03-09 21:00:30 -0300

    animacoes robo

--- src/scenes/maze.js
+++ src/scenes/maze.js
@@ -27,9 +27,12 @@ class Maze extends Phaser.Scene {
     //colisaoLayer.setCollisionBetween(1, 50); // método alternativo de setar colisões
     map.createStaticLayer("decoracao", tileset);
 
-    createAnimation(this, "andar", "player", 0, 3, 3, -1);
-    createAnimation(this, "parado", "player", 1, 1, -1, -1); // funcao gostosa dmais gui vc eh muito lindo
-    createAnimation(this, "engrenagemgira", "engrenagem", 0, 3, 3, -1);
+    createAnimation(this, "parado", "player", 0, 3, 8, -1); // funcao gostosa dmais gui vc eh muito lindo
+    createAnimation(this, "andarBaixo", "player", 4, 7, 8, -1);
+    createAnimation(this, "andarCima", "player", 8, 11, 8, -1);
+    createAnimation(this, "andarDireita", "player", 12, 15, 8, -1);
+    createAnimation(this, "andarEsquerda", "player", 16, 19, 8, -1);
+    createAnimation(this, "engrenagemgira", "engrenagem", 0, 3, 5, -1);
 
     this.portaFechada = this.add
       .image(width / 2, 0, "PortaFechada")
@@ -41,37 +44,42 @@ class Maze extends Phaser.Scene {
     this.player.anims.play("parado");
 
     this.gears = this.physics.add.staticGroup();
-    this.gears.create(width / 2, height / 2, "engrenagem");
-    this.gears.create(88, 500, "engrenagem")
-    this.gears.create(700, 100, "engrenagem")
-    this.gears.create(300, 261, "engrenagem")
-    this.gears.create(700, 500, "engrenagem")
-    this.gears.create(600, 90, "engrenagem")
-    this.gears.create(80, 400, "engrenagem")
-    this.gears.create(290, 146, "engrenagem")
-    this.gears.create(width / 2, 500, "engrenagem")
+
+    let engrenagemPosicoes = [[width / 2, height / 2], [88, 500], [700, 100], [300, 261], [700, 500], [600, 90], [80, 400], [290, 146], [width / 2, 500]]
+
+    for (let i = 0; i < engrenagemPosicoes.length; i++) {
+      this.gears.create(engrenagemPosicoes[i][0], engrenagemPosicoes[i][1], "engrenagem").anims.play('engrenagemgira');
+    }
 
     this.physics.add.overlap(this.player, this.gears, (player, gear) => {
       this.gearsCount++;
       gear.destroy();
     });
+    this.cameras.main.zoom = 1
+    this.cameras.addExisting(this.cameras.main);
+    this.cameras.main.startFollow(this.player);
+    this.player.animState = "idle";
   }
 
   update() {
     if (!this.talking) {
         moveChar(this, 200, 200, this.player); // economizei 3929832999 linhas de codigo aq tmj
     }
-    
 
-    if (this.player.body.velocity.y > 0 || this.player.body.velocity.y < 0 || this.player.body.velocity.x > 0 || this.player.body.velocity.x < 0) {
-      if (!this.andando) {
-        console.log("Toca animação");
-        this.player.anims.play("andar");
-        this.andando = true;
+    if (this.player.body.velocity.y != 0 || this.player.body.velocity.x != 0) {
+      console.log("Toca animação");
+      if (this.player.body.velocity.y > 0) {
+        this.player.animState = "andarBaixo";
+      } else if (this.player.body.velocity.y < 0) {
+        this.player.animState = "andarCima";
+      } else if (this.player.body.velocity.x < 0) {
+        this.player.animState = "andarEsquerda";
+      } else {
+        this.player.animState = "andarDireita"
       }
+      this.andando = true;
     } else {
-        this.player.anims.play('parado')
-        this.andando = false
+        this.player.animState = "parado";
     }
 
     if (this.gearsCount == 9 && !this.aberto) {
@@ -84,6 +92,13 @@ class Maze extends Phaser.Scene {
         this.scene.start("End");
       });
       this.aberto = true;
+    }
+    if (!this.player.body.blocked.left && !this.player.body.blocked.right && !this.player.body.blocked.up && !this.player.body.blocked.down) {
+      this.player.setPosition(Math.round(this.player.x), Math.round(this.player.y))
+    }
+
+    if (this.player.anims.currentAnim != this.player.animState) {
+      this.player.anims.play(this.player.animState, true);
     }
   }
 }
